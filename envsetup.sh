6975340a5ba19444925edbee2901ae0b5e716367
Steve Kondik <shade@chemlab.org>
2013-08-30 17:34:37 -0700

    envsetup: Wait for device in installboot/installrecovery
     * Useful when you're racing against a bug that causes a kernel panic
       shortly after USB turns on :(
    
    Change-Id: I2a503e4b195e632ed5b67a77b942e80c47d0ff45
    
    envsetup: Use wait-for-online so these cmds work in recovery
    
    Change-Id: I7a85e7068f373b1d726aa6b27a51862fa9b7c6fd

--- envsetup.sh
+++ envsetup.sh
@@ -1658,6 +1658,7 @@ function installboot()
         fi
     fi
     adb start-server
+    adb wait-for-online
     adb root
     sleep 1
     adb wait-for-online shell mount /system 2>&1 > /dev/null
@@ -1702,6 +1703,7 @@ function installrecovery()
         fi
     fi
     adb start-server
+    adb wait-for-online
     adb root
     sleep 1
     adb wait-for-online shell mount /system 2>&1 >> /dev/null
